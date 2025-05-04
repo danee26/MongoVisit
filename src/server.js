76ffaef73b9bc0a085221b21c08067813d1f9f59
Dane Donaldson <danedonaldson98@icloud.com>
2025-05-04 19:07:06 +0100

    Added EJS to the project for better html rendering

--- src/server.js
+++ src/server.js
@@ -1,27 +1,40 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const app = express();
+  const express = require('express');
+  const ejs = require('ejs');
+  const { MongoClient } = require('mongodb');
+  const path = require('path');
 
-const port = 6969;
+  
+  const app = express();
+  const port = 3000;
+  const uri = 'mongodb://mongo:27017';
+  const client = new MongoClient(uri);
 
-const uri = 'mongodb://mongo:27017';
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
 
-const client = new MongoClient(uri);
+  app.get('/', async (req, res) => {
+    try {
+      await client.connect();
 
-app.get('/', async (req, res) => {
-  try {
-    await client.connect();
-    const db = client.db('mydb');
-    const time = new Date();
-    await db.collection('visits').insertOne({ time });
-    const visits = await db.collection('visits').find().toArray();
-    res.send(`Visiaated at: ${time}<br>Total visits: ${visits.length}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Something broke!');
-  }
-});
+      const db = client.db('mydb');
+      const time = new Date();
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+      //Get the users IP - I don't want to track visits from a user who has already visited...
+      const ip = req.socket.remoteAddress || req.headers['x-forwarded-for'];
+
+      await db.collection('visits').insertOne({ time, ip });
+
+      const visits = await db.collection('visits').find().toArray();
+
+      
+    res.render('server', {time, visits});
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Something broke!');
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
